47539f90ce4d30858bebcadd1e8cd00d05a93f85
denebSimplify <denebyeoh@simplify.network>
2020-12-04 21:02:33 +0800

    Added page number on story

--- lib/StorySlider.dart
+++ lib/StorySlider.dart
@@ -12,6 +12,7 @@ class StorySlider extends StatefulWidget {
 class _StorySliderState extends State<StorySlider> {
 
   List gingerBread = ["lib/assets/gingerBreadStory/Cover.jpg","lib/assets/gingerBreadStory/Picture1.png","lib/assets/gingerBreadStory/Picture2.png","lib/assets/gingerBreadStory/Picture3.png","lib/assets/gingerBreadStory/Picture4.png","lib/assets/gingerBreadStory/Picture5.png","lib/assets/gingerBreadStory/Picture6.png","lib/assets/gingerBreadStory/Picture7.png","lib/assets/gingerBreadStory/Picture8.png","lib/assets/gingerBreadStory/Picture9.png","lib/assets/gingerBreadStory/Picture10.png","lib/assets/gingerBreadStory/Picture11.png","lib/assets/gingerBreadStory/Picture12.png"];
+  int i = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +52,7 @@ class _StorySliderState extends State<StorySlider> {
             ),
           ),*/
           Container(
-            height: MediaQuery.of(context).size.height - 200,
+            height: MediaQuery.of(context).size.height - 300,
             width: MediaQuery.of(context).size.width,
             //padding: EdgeInsets.all(5),
             child: new Swiper(
@@ -60,16 +61,25 @@ class _StorySliderState extends State<StorySlider> {
                   "http://via.placeholder.com/288x188",
                   fit: BoxFit.fill,
                 );*/
+                //i = index;
                 return Stack(
                   children: <Widget>[
                     Container(
-                      height: MediaQuery.of(context).size.height - 120,
-                      width: MediaQuery.of(context).size.width,
                       decoration: BoxDecoration(
                         image: DecorationImage(
-                          // image: AssetImage(gingerBread[index]),fit: BoxFit.fill,
+                           //image: AssetImage(gingerBread[index]),fit: BoxFit.fill,
                           image:AssetImage(gingerBread[index])
                         ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: EdgeInsets.only(top:(MediaQuery.of(context).size.height - 300)-25),
+                        child: Text("Page ${index+1}",style: new TextStyle(
+                            fontFamily: "Capriola",
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.normal
+                        ),),
                       ),
                     ),
                   ],
@@ -80,6 +90,7 @@ class _StorySliderState extends State<StorySlider> {
               //scale: 0.9,
             ),
           ),
+
         ],
       ),
     );
